ab859dfdb6b3c358aa49058bdb96ccc49fd9e6b3
Lucas Otávio Leme Silva <49615892+lucaspvpa@users.noreply.github.com>
2022-12-07 00:53:54 -0300

    Tela de Registro

--- view/src/LoginScreen.jsx
+++ view/src/LoginScreen.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 function LoginScreen(props) {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
+    const [validityEmail, setValidityEmail] = useState(false);
     const [password, setPassword] = useState("");
-    const [userType, setUserType] = useState();
+    const [userType, setUserType] = useState(false);
     const [error, setError] = useState("");
     const [register, setRegister] = useState(false);
+    const [successRegister, setSuccessRegister] = useState(false);
     return (
         !register ? <div className="container">
             <div className="login-screen">
@@ -14,6 +16,7 @@ function LoginScreen(props) {
                     <h1>Entrar</h1>
                     <button onClick={() => props.setLoginScreenOn(false)}>X</button>
                 </div>
+                {successRegister && <h4>Usuário cadastrado com sucesso!</h4>}
                 <div className="form">
                     <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
@@ -44,7 +47,7 @@ function LoginScreen(props) {
                     }}>Entrar</button>
                     <button type="button" onClick={() => {
                         setRegister(true)
-                    }}>Registrar</button>
+                    }}>Registre-se</button>
                 </div>
             </div>
         </div> : <div className="container">
@@ -55,13 +58,27 @@ function LoginScreen(props) {
                 </div>
                 <div className="form">
                     <input type="text" placeholder="Nome de usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => {
+                        setValidityEmail(e.target.reportValidity())
+                        setEmail(e.target.value)
+                    }} />
                     <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <input type="checkbox" value='admin' onChange={e => { setUserType(e.target.value) }} />
+                    <div className="checkbox-admin">
+                        <input type="checkbox" value='admin' onChange={e => { setUserType(e.target.checked) }} />
+                        <span> Administrador</span>
+                    </div>
                     {error !== "" && <p>{error}</p>}
                     <button type="button" onClick={() => {
+                        if (username.length < 3) {
+                            setError("Nome de usuário deve ter no mínimo 3 caracteres");
+                            return;
+                        }
                         if (email.length < 3) {
                             setError("Email deve ter no mínimo 3 caracteres");
+                            return;
+                        }
+                        if (!validityEmail) {
+                            setError("Email inválido");
                             return;
                         }
                         if (password.length < 3) {
@@ -71,9 +88,19 @@ function LoginScreen(props) {
                         fetch("/usuario/cadastrar", {
                             method: "POST",
                             headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify({ email, password, username, userType })
+                            body: JSON.stringify({ email, senha: password, nome: username, tipo: userType ? 'admin' : 'usuario' })
                         }).then(response => response.json()).then(res => {
-
+                            if (res.erro) {
+                                setError(res.erro)
+                            } else {
+                                setEmail("")
+                                setPassword("")
+                                setUsername("")
+                                setUserType(false)
+                                setError("")
+                                setSuccessRegister(true)
+                                setRegister(false)
+                            }
                         })
                     }}>Registrar</button>
                 </div>
